874cb0b2fb511822314471c49e0d324051abd2df
shrisrivastav <shrijitsrivastav@gmail.com>
2024-03-04 01:11:48 +0530

    day 1

---
+++ 1. basics/app.js
@@ -0,0 +1,43 @@
+//to find the data type of variables
+// let n = 100;
+// console.log(typeof n);
+
+//----------------------------
+
+
+//  === checks type and value
+//  == checks only value
+
+//---------------------------------
+
+//tochange cases
+// .tolowercase
+// .tpuppercase 
+
+//----------------------------------
+
+// let s = "shrijit"
+
+// .slice(inclusive,exclusive)
+// s= s.slice(0,3)
+// console.log(s)
+
+
+//--------------------------------------
+//  //splits string into array
+
+// let s = "shrijit"
+//  .split(separator)
+// console.log(s.split(""))
+
+// .join("") will join back
+//---------------------------------------
+
+//to change to int
+
+// s = parseInt(s);
+
+// s=s.string()
+// ---------------------------------------
+
+
